2eb5de9f0011ae713300dd0aeb468d348a618fd7
Alec Armbruster <35377827+alectrocute@users.noreply.github.com>
2020-04-20 17:47:48 -0700

    Cleaning 🧼

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 var os = require("os");
 var path = require("path");
 var platform = os.platform();
+var arch = os.arch();
+
 if (platform == "darwin") {
   platform = "mac";
 } else if (platform == "win32") {
   platform = "win";
 }
+
 let file = platform == "mac" ? "HandBrakeCLI" : "HandBrakeCLI.exe";
+
+// linux coming soon
 if (platform !== "mac" && platform !== "win") {
   process.exit(1);
 }
-var arch = os.arch();
+
 var productionPath = path
   .join(__dirname, `node_modules/handbrake-static/bin/${platform}/x64/${file}`)
   .replace("app.asar", "app.asar.unpacked");
